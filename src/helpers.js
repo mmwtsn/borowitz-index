07f2052f3a701828695d32d7ab5e8a35163e0bc3
M. Maxwell Watson <hi@mmwtsn.com>
2015-10-17 16:39:33 -0400

    Document helpers.parse()

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,10 @@
-export function parse () {
+/**
+ * Parses the New Yorker's homepage HTML and returns the number of stories
+ * written by Andy Borowitz from the "Most Read" list.
+ *
+ * @param {string} html
+ * @returns {number}
+ */
+export function parse (html) {
   return 0
 }
