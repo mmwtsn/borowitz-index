d095e14e6da91cab7b10d59960238a727ec4d5a6
M. Maxwell Watson <hi@mmwtsn.com>
2015-10-17 17:55:02 -0400

    Replace stubbed count

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import request from 'request'
+import {parse, count} from './helpers'
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({ borowitz_index: 0 })
+  request('http://www.newyorker.com', (err, response, html) => {
+    if (err || response.statusCode !== 200) throw err
+
+    res.json({ borowitz_index: count(parse(html)) })
+  })
 })
 
 export default app
