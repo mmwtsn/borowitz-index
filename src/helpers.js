cbb39eee4338eca57b44ef428f502cc1c798b26c
M. Maxwell Watson <hi@mmwtsn.com>
2015-10-17 17:38:49 -0400

    Implement helpers.parse()

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+import cheerio from 'cheerio'
+
 /**
  * Parses the New Yorker's homepage HTML and returns the number of stories
  * written by Andy Borowitz from the "Most Read" list.
@@ -6,5 +8,11 @@
  * @returns {number}
  */
 export function parse (html) {
-  return 0
+  const $ = cheerio.load(html)
+
+  const authors = $('#most-popular h3 a').map((i, el) => {
+    return el.children[0].data
+  }).get()
+
+  return authors.filter(e => e.match(/Borowitz/)).length
 }
